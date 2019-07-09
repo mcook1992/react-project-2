a8daefbba1f3f6a0d25133206b089c82904ace56
Mercer Cook <mercercook@Mercers-MacBook-Pro.local>
2019-07-09 16:24:17 -0400

    testing for heroku

--- react-backend/app.js
+++ react-backend/app.js
@@ -23,7 +23,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 // app.use(express.static(path.join(__dirname, "public")));
 
-express.static(path_join(__dirname, "./client/build"));
+express.static(path.join(__dirname, "./client/build"));
 //passport stuff
 app.use(express.static("public"));
 app.use(session({ secret: "cats" }));
