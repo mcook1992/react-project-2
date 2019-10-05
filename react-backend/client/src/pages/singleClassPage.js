eaf53acf74bf581a7ef1b28a061af04428f9a8b8
Mercer Cook <mercercook@dyn-160-39-213-232.dyn.columbia.edu>
2019-10-05 19:36:27 -0400

    got data showing on teacher page but need to improve it

--- react-backend/client/src/pages/singleClassPage.js
+++ react-backend/client/src/pages/singleClassPage.js
@@ -1,5 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend
+} from "recharts";
 
 class singleClassPage extends React.Component {
   constructor(props) {
@@ -7,7 +16,7 @@ class singleClassPage extends React.Component {
     this.state = {
       className: this.props.location.pathname.replace("/classes/", ""),
       studentArray: [],
-      data: []
+      stressLevelOverTimeArray: []
     };
   }
 
@@ -21,7 +30,23 @@ class singleClassPage extends React.Component {
       .then(res => res.json())
       .then(data => {
         console.log(data);
-        this.setState({ studentArray: data.studentNameArray });
+        var studentStressLevels = [];
+        //start with individual student
+        data.data.forEach(element => {
+          //now add each answer separately--don't have to keep students apart for this particular answer
+          element.forEach(elem => {
+            const newObject = {
+              dateSubmitted: elem.dateSubmitted,
+              answer: elem.quizQuestions[0].value
+            };
+            studentStressLevels.push(newObject);
+          });
+        });
+        console.log("Student stress level array is" + studentStressLevels);
+        this.setState({
+          studentArray: data.studentNameArray,
+          stressLevelOverTimeArray: studentStressLevels
+        });
       });
   }
   render() {
@@ -36,6 +61,28 @@ class singleClassPage extends React.Component {
           ))}
         </div>
         <h2>Class Data</h2>
+
+        <div>
+          <LineChart
+            width={300}
+            height={300}
+            data={this.state.stressLevelOverTimeArray}
+            margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+          >
+            <XAxis dataKey="dateSubmitted" />
+            <YAxis type="number" domain={[0, 4]} />
+            <CartesianGrid strokeDasharray="3 4" />
+            <Tooltip />
+            <Legend />
+            <Line
+              type="monotone"
+              dataKey="answer"
+              stroke="#8884d8"
+              activeDot={{ r: 8 }}
+            />
+            {/* <Line type="monotone" dataKey="uv" stroke="#82ca9d" /> */}
+          </LineChart>
+        </div>
       </div>
     );
   }
